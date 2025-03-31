c0e0146ade0e753b459786cbe905c7ae3812a888
Abdullah Al Muzaki <aamuzakii@gmail.com>
2025-03-31 14:05:00 +0700

    adjust json shape

--- api/jobs.js
+++ api/jobs.js
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'src', 'jobs.json');
+  const filePath = path.join(process.cwd(), 'src', 'jobs-2.json');
   const data = JSON.parse(readFileSync(filePath, 'utf-8'));
 
   res.status(200).json(data);

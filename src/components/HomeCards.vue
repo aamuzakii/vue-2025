ad24d2474514b1b5a57bfb3ea768d3403b6184c4
Abdullah Al Muzaki <aamuzakii@gmail.com>
2025-03-31 22:24:01 +0700

    more data

--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -10,7 +10,7 @@ import Card from '@/components/Card.vue';
         <Card>
           <h2 class="text-2xl font-bold">For Developers</h2>
           <p class="mt-2 mb-4">
-            Browse our Vue jobs and start your career today
+            Browse our Vue Property and start your career today
           </p>
           <RouterLink
             to="/jobs"
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -94,9 +94,9 @@ const isActiveLink = (routePath) => {
         >
           <!-- Logo -->
           <RouterLink class="flex flex-shrink-0 items-center mr-4" to="/">
-            <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
+            <img class="h-10 w-auto" :src="logo" alt="Vue Property" />
             <span class="hidden md:block text-black text-2xl font-bold ml-2"
-              >Vue Jobs</span
+              >Vue Property</span
             >
           </RouterLink>
           <div class="md:ml-auto flex items-center space-x-2">

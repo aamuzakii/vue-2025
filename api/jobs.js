b7a1cd54f63d0395fbd59853c4e2f162873c4aa4
Abdullah Al Muzaki <aamuzakii@gmail.com>
2025-03-31 14:27:26 +0700

    use single json

--- api/jobs.js
+++ api/jobs.js
@@ -2,8 +2,8 @@ import { readFileSync } from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'src', 'jobs-2.json');
+  const filePath = path.join(process.cwd(), 'src', 'jobs.json');
   const data = JSON.parse(readFileSync(filePath, 'utf-8'));
 
-  res.status(200).json(data);
+  res.status(200).json(data.jobs);
 }

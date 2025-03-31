fd6412d9745f533414975dd8b390d999601c9647
Abdullah Al Muzaki <aamuzakii@gmail.com>
2025-03-31 15:18:54 +0700

    rename to prop

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -19,14 +19,14 @@ const state = reactive({
 
 const deleteJob = async () => {
   try {
-    const confirm = window.confirm('Are you sure you want to delete this job?');
+    const confirm = window.confirm('Are you sure you want to delete this properties?');
     if (confirm) {
       await axios.delete(`/api/properties/${jobId}`);
       toast.success('Job Deleted Successfully');
       router.push('/properties');
     }
   } catch (error) {
-    console.error('Error deleting job', error);
+    console.error('Error deleting properties', error);
     toast.error('Job Not Deleted');
   }
 };
@@ -34,9 +34,9 @@ const deleteJob = async () => {
 onMounted(async () => {
   try {
     const response = await axios.get(`/api/properties/${jobId}`);
-    state.job = response.data;
+    state.properties = response.data;
   } catch (error) {
-    console.error('Error fetching job', error);
+    console.error('Error fetching properties', error);
   } finally {
     state.isLoading = false;
   }
@@ -52,13 +52,13 @@ onMounted(async () => {
           <div
             class="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
           >
-            <div class="text-gray-500 mb-4">{{ state.job.type }}</div>
-            <h1 class="text-3xl font-bold mb-4">{{ state.job.title }}</h1>
+            <div class="text-gray-500 mb-4">{{ state.properties.type }}</div>
+            <h1 class="text-3xl font-bold mb-4">{{ state.properties.title }}</h1>
             <div
               class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
             >
               <i class="pi pi-map-marker text-xl text-orange-700 mr-2"></i>
-              <p class="text-orange-700">{{ state.job.address }}</p>
+              <p class="text-orange-700">{{ state.properties.address }}</p>
             </div>
           </div>
 
@@ -68,12 +68,12 @@ onMounted(async () => {
             </h3>
 
             <p class="mb-4">
-              {{ state.job.description }}
+              {{ state.properties.description }}
             </p>
 
             <h3 class="text-green-800 text-lg font-bold mb-2">Salary</h3>
 
-            <p class="mb-4">{{ state.job.salary }} / Year</p>
+            <p class="mb-4">{{ state.properties.salary }} / Year</p>
           </div>
         </main>
 
@@ -83,10 +83,10 @@ onMounted(async () => {
           <div class="bg-white p-6 rounded-lg shadow-md">
             <h3 class="text-xl font-bold mb-6">Company Info</h3>
 
-            <!-- <h2 class="text-2xl">{{ state.job.company.name }}</h2> -->
+            <!-- <h2 class="text-2xl">{{ state.properties.company.name }}</h2> -->
 
             <p class="my-2">
-              <!-- {{ state.job.company.description }} -->
+              <!-- {{ state.properties.company.description }} -->
             </p>
 
             <hr class="my-4" />
@@ -94,14 +94,14 @@ onMounted(async () => {
             <h3 class="text-xl">Contact Email:</h3>
 
             <p class="my-2 bg-green-100 p-2 font-bold">
-              <!-- {{ state.job.company.contactEmail }} -->
+              <!-- {{ state.properties.company.contactEmail }} -->
                 aamuzakii@gmail.com
             </p>
 
             <h3 class="text-xl">Contact Phone:</h3>
 
             <p class="my-2 bg-green-100 p-2 font-bold">
-              <!-- {{ state.job.company.contactPhone }} -->
+              <!-- {{ state.properties.company.contactPhone }} -->
                 + 62 838 9458 8105
             </p>
           </div>
@@ -110,7 +110,7 @@ onMounted(async () => {
           <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-xl font-bold mb-6">Manage Job</h3>
             <RouterLink
-              :to="`/jobs/edit/${state.job.id}`"
+              :to="`/jobs/edit/${state.properties.id}`"
               class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >Edit Job</RouterLink
             >
